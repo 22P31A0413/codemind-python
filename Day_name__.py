d7d768eef761b0ee534e063a10371e9faf2433a3
22P31A0413 <119718730+22P31A0413@users.noreply.github.com>
2023-08-20 12:25:12 +0530

    wd=int(input()) if(wd==1):     print("Monday") elif(wd==2):     print("Tuesday") elif(wd==3):     print("Wednesday") elif(wd==4):     print("Thursday") elif(wd==5):     print("Friday") elif(wd==6):     print("Saturday") elif(wd==7):     print("Sunday") else:     print("Invalid input")

---
+++ Day_name__.py
@@ -0,0 +1,17 @@
+wd=int(input())
+if(wd==1):
+    print("Monday")
+elif(wd==2):
+    print("Tuesday")
+elif(wd==3):
+    print("Wednesday")
+elif(wd==4):
+    print("Thursday")
+elif(wd==5):
+    print("Friday")
+elif(wd==6):
+    print("Saturday")
+elif(wd==7):
+    print("Sunday")
+else:
+    print("Invalid input")
